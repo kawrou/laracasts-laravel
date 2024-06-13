16f16e8d2cc5b27140e94b242eb8a2e101db4853
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-13 18:46:01 +0100

    Started building out a webpage following tutorial on laracasts

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
+    <h1>Welcome to the contact page</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+    
+    <h1>Welcome to the main page</h1>
+</x-layout>
