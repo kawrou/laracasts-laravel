4273ea93477de316c7758ba02867c1121d9361b0
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-21 19:31:19 +0100

    Wrote a 'create job' feature

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/jobs', function () {
     //eager loading to solve n+1 problem
     //Paginate so that not all the data is displayed at once
     // $jobs = Job::with('employer')->paginate(6);
-    $jobs = Job::with('employer')->simplePaginate(6);  
+    $jobs = Job::with('employer')->latest()->simplePaginate(6);
     // $jobs = Job::with('employer')->cursorPaginate(3);  
 
     //Lazy loading
@@ -31,7 +31,15 @@ Route::get('/jobs/{id}', function ($id) {
     return view('Jobs.show', ['job' => $job]);
 });
 
+Route::post('/jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
 
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function () {
     return view('contact');
