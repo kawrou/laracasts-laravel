5183b9c7b89e760cd4656c7eca095f22d5457d91
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-24 15:48:32 +0100

    Refactored routes to use Route::controller() and Route::resource()

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,30 @@
 use App\Http\Controllers\JobsController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-
-Route::controller(JobsController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
-
-
 //Can use Route:view when returning a static page or just a view
+Route::view('/', 'home');
 Route::view('/contact', 'contact');
+
+// You can group all of your routes using Route::controller() and pass
+// the controller as arg
+// Route::controller(JobsController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
+
+//Or you can use Route::resource(); 
+//Registers all of the routes for a typical resourceful controller
+//You'll still need to follow the conventions of:
+//[index, create, show, store, edit, update, destroy]
+//in your controller, otherwise it'll throw an error
+Route::resource('jobs', JobsController::class, [
+    // 'only'  => ['edit'], 
+    // 'except'=> ['edit'], 
+]);
+
+
