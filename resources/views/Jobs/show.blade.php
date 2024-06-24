fdcc4f4eaaee7fd518d6c194c4b7794d34785692
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-24 14:23:29 +0100

    Created job edit page. Updated routes with a patch request route

--- resources/views/Jobs/show.blade.php
+++ resources/views/Jobs/show.blade.php
@@ -3,11 +3,11 @@
         Job 
     </x-slot:heading>
 
-    <h2><strong>{{$job['title']}}</strong></h2>
+    <h2><strong>{{$job->title}}</strong></h2>
 
-    <p>The job pays {{$job['salary']}}</p>
+    <p>The job pays {{$job->salary}}</p>
 
     <p class="mt-6">
-        <x-button href="/jobs/edit">Edit Job</x-button>
+        <x-button href="/jobs/{{$job->id}}/edit">Edit Job</x-button>
     </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     //eager loading to solve n+1 problem
     //Paginate so that not all the data is displayed at once
@@ -22,15 +23,18 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create 
 Route::get('/jobs/create', function () {
     return view('Jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('Jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -44,6 +48,37 @@ Route::post('/jobs', function () {
     ]);
 
     return redirect('/jobs');
+});
+
+//Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('Jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+    // authorize
+
+    //findOrFail(): to mitigate against searching for a job that doesn't exist
+    //Will throw a model not found exception
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize
 });
 
 Route::get('/contact', function () {
