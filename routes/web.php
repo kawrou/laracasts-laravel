a06467ec9e452167546d5d04da1e6b60a6491101
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-24 18:26:05 +0100

    Used reuseable component to create a login and register page view. Created the routes for those views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SessionController; 
+use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\JobsController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,4 +31,8 @@ Route::resource('jobs', JobsController::class, [
     // 'except'=> ['edit'], 
 ]);
 
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
+Route::get('/login', [SessionController::class,'create']);    
+Route::post('/login', [SessionController::class,'store']);    
